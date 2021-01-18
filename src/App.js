5a03e36881a5249a639b5e4d13c8b222c4b36f63
gabogarciam <gabogarciam71@gmail.com>
2021-01-19 00:15:15 +0100

    create and configure basic structure for a react project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.scss";
+
+const App = () => (
+  <h1>Welcome to the covid 19 tracker project with React!!!</h1>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  mode: "production",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
